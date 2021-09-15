8172d9653b67ab0fa6185dbf611b112ec7cf5138
Abdulbaki Dursun <eggsydev@gmail.com>
2021-09-15 22:44:54 +0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Nuxt Hello World
+
+⚡️ Lightning fast, and advanced Nuxt.js module for those who want to see "hello world" in their console every time they change a page, or start their Nuxt server.
+
+Just add it to your `nuxt.config.js`file:
+
+```js
+export default {
+  buildModules: ["nuxt-hello-world"],
+};
+```
+
+You'll see a new badge displaying when you run your Nuxt app, that means the module is working correctly.
---
+++ pages/hello-world.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h1>Hello World!</h1>
+
+    <p>
+      A module by
+      <a href="https://github.com/eggsy" rel="noreferrer" target="_blank">
+        @eggsy
+      </a>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 95vh;
+}
+
+a {
+  color: #3b5998;
+  text-decoration: underline;
+}
+</style>
